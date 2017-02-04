be590c6794a8abce3a3a67e5f250f776a3028cfc
Tyler Hall <tylerwhall@gmail.com>
2017-02-04 16:48:51 -0500

    init

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,53 @@
 extern crate spidev;
 
 use spidev::{Spidev, SpidevOptions, SpidevTransfer};
-use std::io;
+use std::io::{self, Write};
+
+struct RfmRegs {
+    spi: Spidev,
+}
+
+impl RfmRegs {
+    fn new(spi: Spidev) -> Self {
+        RfmRegs { spi: spi }
+    }
+
+    fn read(&mut self, reg: u8) -> io::Result<u8> {
+        let mut rbuf = [0u8, 0u8];
+        let tbuf = [reg, 0u8];
+        self.spi.transfer(&mut SpidevTransfer::read_write(&tbuf, &mut rbuf)).map(|_| rbuf[1])
+    }
+
+    fn write(&mut self, reg: u8, val: u8) -> io::Result<()> {
+        self.spi.write_all(&[reg | 0x80, val])
+    }
+}
+
+#[repr(u8)]
+enum Rfm22Regs {
+    OperatingFunctionControl1 = 0x7,
+}
 
 struct Rfm22 {
-    spi: Spidev,
+    regs: RfmRegs,
 }
 
 impl Rfm22 {
     fn new(spi: Spidev) -> Self {
-        Rfm22 { spi: spi }
+        Rfm22 { regs: RfmRegs::new(spi) }
     }
 
-    fn read_reg(&mut self, reg: u8) -> io::Result<u8> {
-        let mut rbuf = [0u8, 0u8];
-        let tbuf = [reg, 0u8];
-        self.spi.transfer(&mut SpidevTransfer::read_write(&tbuf, &mut rbuf)).map(|_| rbuf[1])
+    fn read(&mut self, reg: Rfm22Regs) -> io::Result<u8> {
+        self.regs.read(reg as u8)
+    }
+
+    fn write(&mut self, reg: Rfm22Regs, val: u8) -> io::Result<()> {
+        self.regs.write(reg as u8, val)
+    }
+
+    pub fn init(&mut self) {
+        self.write(Rfm22Regs::OperatingFunctionControl1, 0x3).unwrap();
+        assert_eq!(self.read(Rfm22Regs::OperatingFunctionControl1).unwrap(), 0x3);
     }
 }
 
@@ -27,5 +59,5 @@ fn main() {
     spi.configure(&options).unwrap();
 
     let mut rf = Rfm22::new(spi);
-    println!("Hello, world! {:#x}", rf.read_reg(0x7).unwrap());
+    rf.init();
 }
