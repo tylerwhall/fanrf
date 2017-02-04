a3449f743c6bd7cdcdcda44054e8397d03e3651e
Tyler Hall <tylerwhall@gmail.com>
2017-02-04 21:26:56 +0000

    10MHz read works

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 extern crate spidev;
 
-use spidev::{Spidev, SpidevTransfer};
+use spidev::{Spidev, SpidevOptions, SpidevTransfer};
 
 fn main() {
-    let spi = Spidev::open("/dev/spidev1.0").unwrap();
-    let mut rbuf = [0u8];
-    let tbuf  = [0x7u8];
+    let mut spi = Spidev::open("/dev/spidev1.0").unwrap();
+    let options = SpidevOptions::new()
+        .max_speed_hz(10 * 1000 * 1000)
+        .build();
+    spi.configure(&options).unwrap();
+    let mut rbuf = [0u8, 0u8];
+    let tbuf  = [0x12u8, 0u8];
     {
         let mut t = SpidevTransfer::read_write(&tbuf, &mut rbuf);
         spi.transfer(&mut t).unwrap();
     }
-    println!("Hello, world! {:#x}", rbuf[0]);
+    println!("Hello, world! {:#x}", rbuf[1]);
 }
