dcea3c5fc51431a5beaacb12e1eb75f9ab6745c2
Tyler Hall <tylerwhall@gmail.com>
2017-02-04 16:36:07 -0500

    RF struct

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 extern crate spidev;
 
 use spidev::{Spidev, SpidevOptions, SpidevTransfer};
+use std::io;
+
+struct Rfm22 {
+    spi: Spidev,
+}
+
+impl Rfm22 {
+    fn new(spi: Spidev) -> Self {
+        Rfm22 { spi: spi }
+    }
+
+    fn read_reg(&mut self, reg: u8) -> io::Result<u8> {
+        let mut rbuf = [0u8, 0u8];
+        let tbuf = [reg, 0u8];
+        self.spi.transfer(&mut SpidevTransfer::read_write(&tbuf, &mut rbuf)).map(|_| rbuf[1])
+    }
+}
 
 fn main() {
     let mut spi = Spidev::open("/dev/spidev1.0").unwrap();
@@ -8,11 +25,7 @@ fn main() {
         .max_speed_hz(10 * 1000 * 1000)
         .build();
     spi.configure(&options).unwrap();
-    let mut rbuf = [0u8, 0u8];
-    let tbuf  = [0x12u8, 0u8];
-    {
-        let mut t = SpidevTransfer::read_write(&tbuf, &mut rbuf);
-        spi.transfer(&mut t).unwrap();
-    }
-    println!("Hello, world! {:#x}", rbuf[1]);
+
+    let mut rf = Rfm22::new(spi);
+    println!("Hello, world! {:#x}", rf.read_reg(0x7).unwrap());
 }
