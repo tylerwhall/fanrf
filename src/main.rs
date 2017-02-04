64d7e0fcb440eaed7f21ee77fe4535735b833e7e
Tyler Hall <tylerwhall@gmail.com>
2017-02-04 18:41:13 -0500

    Set modulation type

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+#[macro_use]
+extern crate bitflags;
 extern crate spidev;
 
-use spidev::{Spidev, SpidevOptions, SpidevTransfer};
+use std::fmt::Debug;
 use std::io::{self, Write};
+
+use spidev::{Spidev, SpidevOptions, SpidevTransfer};
 
 struct RfmRegs {
     spi: Spidev,
@@ -23,9 +27,110 @@ impl RfmRegs {
     }
 }
 
+trait RfmReg {
+    /// Get the register number for this reg struct
+    fn regval() -> u8;
+}
+
 #[repr(u8)]
-enum Rfm22Regs {
+#[derive(Clone, Copy)]
+enum Rfm22RegVal {
     OperatingFunctionControl1 = 0x7,
+    ModulationModeControl1 = 0x70,
+    ModulationModeControl2 = 0x71,
+}
+
+trait Rfm22Reg: Sized + PartialEq + Debug + Copy {
+    fn reg() -> Rfm22RegVal;
+
+    fn from_bits(bits: u8) -> Option<Self>;
+    fn bits(&self) -> u8;
+}
+
+impl<R: Rfm22Reg> RfmReg for R {
+    fn regval() -> u8 {
+        R::reg() as u8
+    }
+}
+
+macro_rules! rfreg {
+    ( $name:ident { $($flag:ident = $value:expr),+ } ) => {
+        bitflags! {
+            flags $name: u8 {
+                $(const $flag = (1 << $value)),+
+            }
+        }
+
+        impl Rfm22Reg for $name {
+            fn reg() -> Rfm22RegVal {
+                Rfm22RegVal::$name
+            }
+
+            #[inline(always)]
+            fn from_bits(bits: u8) -> Option<Self> {
+                Self::from_bits(bits)
+            }
+
+            #[inline(always)]
+            fn bits(&self) -> u8 {
+                self.bits()
+            }
+        }
+    };
+}
+
+rfreg! {
+    OperatingFunctionControl1 {
+        XTON = 0,
+        PLLON = 1,
+        RXON = 2,
+        TXON = 3,
+        X32KSEL = 4,
+        ENWT = 5,
+        ENLBD = 6,
+        SWRES = 7
+    }
+}
+rfreg! {
+    ModulationModeControl1 {
+        ENWHITE = 0,
+        ENMANCH = 1,
+        ENMANINV = 2,
+        MANPPOL = 3,
+        ENPHPWDN = 4,
+        TXDRTSCALE = 5
+    }
+}
+rfreg! {
+    ModulationModeControl2 {
+        MODTYP0 = 0,
+        MODTYP1 = 1,
+        FD8 = 2,
+        ENINV = 3,
+        DTMOD0 = 4,
+        DTMOD1 = 5,
+        TRCLK0 = 6,
+        TRCLK1 = 7
+    }
+}
+
+pub enum ModulationType {
+    Unmodulated,
+    OOK,
+    FSK,
+    GFSK,
+}
+
+impl ModulationModeControl2 {
+    pub fn set_modtype(&mut self, ty: ModulationType) {
+        self.remove(MODTYP0 | MODTYP1);
+        self.insert(match ty {
+            ModulationType::Unmodulated => Self::empty(),
+            ModulationType::OOK => MODTYP0,
+            ModulationType::FSK => MODTYP1,
+            ModulationType::GFSK => MODTYP1 | MODTYP0,
+        });
+    }
 }
 
 struct Rfm22 {
@@ -37,17 +142,28 @@ impl Rfm22 {
         Rfm22 { regs: RfmRegs::new(spi) }
     }
 
-    fn read(&mut self, reg: Rfm22Regs) -> io::Result<u8> {
-        self.regs.read(reg as u8)
+    fn read<R: Rfm22Reg>(&mut self) -> io::Result<R> {
+        self.regs.read(R::regval()).map(|val| R::from_bits(val).unwrap())
     }
 
-    fn write(&mut self, reg: Rfm22Regs, val: u8) -> io::Result<()> {
-        self.regs.write(reg as u8, val)
+    fn write<R: Rfm22Reg>(&mut self, val: R) -> io::Result<()> {
+        self.regs.write(R::regval(), val.bits())
+    }
+
+    fn write_validate<R: Rfm22Reg>(&mut self, val: R) -> io::Result<()> {
+        self.write(val)?;
+        assert_eq!(val, self.read().unwrap());
+        Ok(())
+    }
+
+    fn set_modulation_type(&mut self, ty: ModulationType) -> io::Result<()> {
+        let mut reg: ModulationModeControl2 = self.read()?;
+        reg.set_modtype(ty);
+        self.write_validate(reg)
     }
 
     pub fn init(&mut self) {
-        self.write(Rfm22Regs::OperatingFunctionControl1, 0x3).unwrap();
-        assert_eq!(self.read(Rfm22Regs::OperatingFunctionControl1).unwrap(), 0x3);
+        self.write_validate(XTON | PLLON).unwrap();
     }
 }
 
@@ -60,4 +176,5 @@ fn main() {
 
     let mut rf = Rfm22::new(spi);
     rf.init();
+    rf.set_modulation_type(ModulationType::OOK).unwrap();
 }
