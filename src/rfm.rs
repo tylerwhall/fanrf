12fa644ee82e27f5007ea75082ecb9b9407ca4c8
Tyler Hall <tylerwhall@gmail.com>
2017-02-16 18:53:18 -0500

    Move rfm code to separate file

---
+++ src/rfm.rs
@@ -0,0 +1,719 @@
+use std::fmt::Debug;
+use std::io::{self, Write};
+use std::ops::DerefMut;
+use std::thread;
+use std::time::Duration;
+
+use spidev::{Spidev, SpidevTransfer};
+
+const FIFO_SIZE: usize = 64;
+
+trait RegRw {
+    fn read(&mut self, reg: u8) -> io::Result<u8>;
+    fn write(&mut self, reg: u8, val: u8) -> io::Result<()>;
+    fn burst_write(&mut self, reg: u8, val: &[u8]) -> io::Result<()>;
+}
+
+// Not sure why this is required
+impl<T: RegRw + ?Sized> RegRw for Box<T> {
+    fn read(&mut self, reg: u8) -> io::Result<u8> {
+        self.deref_mut().read(reg)
+    }
+    fn write(&mut self, reg: u8, val: u8) -> io::Result<()> {
+        self.deref_mut().write(reg, val)
+    }
+    fn burst_write(&mut self, reg: u8, val: &[u8]) -> io::Result<()> {
+        self.deref_mut().burst_write(reg, val)
+    }
+}
+
+struct RfmRegs {
+    spi: Spidev,
+}
+
+impl RfmRegs {
+    fn new(spi: Spidev) -> Self {
+        RfmRegs { spi: spi }
+    }
+}
+
+impl RegRw for RfmRegs {
+    fn read(&mut self, reg: u8) -> io::Result<u8> {
+        let mut rbuf = [0u8, 0u8];
+        let tbuf = [reg, 0u8];
+        self.spi.transfer(&mut SpidevTransfer::read_write(&tbuf, &mut rbuf)).map(|_| rbuf[1])
+    }
+
+    fn write(&mut self, reg: u8, val: u8) -> io::Result<()> {
+        self.spi.write_all(&[reg | 0x80, val])
+    }
+
+    fn burst_write(&mut self, reg: u8, val: &[u8]) -> io::Result<()> {
+        let addr = [reg | 0x80];
+        let mut tx = [SpidevTransfer::write(&addr), SpidevTransfer::write(val)];
+        self.spi.transfer_multiple(&mut tx)
+    }
+}
+
+struct FakeRegs([u8; 0x80]);
+
+impl FakeRegs {
+    fn new() -> Self {
+        FakeRegs([0; 128])
+    }
+}
+
+impl RegRw for FakeRegs {
+    fn read(&mut self, reg: u8) -> io::Result<u8> {
+        Ok(self.0[reg as usize])
+    }
+
+    fn write(&mut self, reg: u8, val: u8) -> io::Result<()> {
+        self.0[reg as usize] = val;
+        Ok(())
+    }
+
+    fn burst_write(&mut self, mut reg: u8, val: &[u8]) -> io::Result<()> {
+        for byte in val {
+            self.0[reg as usize] = *byte;
+            if reg < 0x7f {
+                // Auto-increment unless this is the fifo register
+                reg += 1;
+            }
+        }
+        Ok(())
+    }
+}
+
+struct RegLogger<R: RegRw>(pub R);
+
+impl<R: RegRw> RegRw for RegLogger<R> {
+    fn read(&mut self, reg: u8) -> io::Result<u8> {
+        self.0.read(reg).map(|val| {
+            println!("Reg read  0x{:02x} = 0x{:02x}", reg, val);
+            val
+        })
+    }
+
+    fn write(&mut self, reg: u8, val: u8) -> io::Result<()> {
+        println!("Reg write 0x{:02x} = 0x{:02x}", reg, val);
+        self.0.write(reg, val)
+    }
+
+    fn burst_write(&mut self, reg: u8, val: &[u8]) -> io::Result<()> {
+        println!("Burst({:2}) 0x{:02x} = {:?}", val.len(), reg, val);
+        self.0.burst_write(reg, val)
+    }
+}
+
+trait RfmReg {
+    /// Get the register number for this reg struct
+    fn regval() -> u8;
+}
+
+#[repr(u8)]
+#[derive(Clone, Copy)]
+pub enum Rfm22RegVal {
+    InterruptStatus1 = 0x3,
+    InterruptStatus2 = 0x4,
+    InterruptEnable1 = 0x5,
+    InterruptEnable2 = 0x6,
+    OperatingFunctionControl1 = 0x7,
+    OperatingFunctionControl2 = 0x8,
+    DataAccessControl = 0x30,
+    HeaderControl2 = 0x33,
+    TxPower = 0x6d,
+    TxDataRate1 = 0x6e,
+    TxDataRate0 = 0x6f,
+    ModulationModeControl1 = 0x70,
+    ModulationModeControl2 = 0x71,
+    FrequencyOffset1 = 0x73,
+    FrequencyOffset2 = 0x74,
+    FrequencyBandSelect = 0x75,
+    CarrierFrequency1 = 0x76,
+    CarrierFrequency0 = 0x77,
+    FIFOAccess = 0x7f,
+}
+
+pub trait Rfm22Reg: Sized + PartialEq + Debug + Copy {
+    fn reg() -> Rfm22RegVal;
+
+    fn from_bits(bits: u8) -> Option<Self>;
+    fn bits(&self) -> u8;
+}
+
+impl<R: Rfm22Reg> RfmReg for R {
+    fn regval() -> u8 {
+        R::reg() as u8
+    }
+}
+
+macro_rules! rfreg {
+    ( $name:ident { $($flag:ident = $value:expr),+ } ) => {
+        bitflags! {
+            pub flags $name: u8 {
+                $(const $flag = (1 << $value)),+
+            }
+        }
+
+        impl Rfm22Reg for $name {
+            fn reg() -> Rfm22RegVal {
+                Rfm22RegVal::$name
+            }
+
+            #[inline(always)]
+            fn from_bits(bits: u8) -> Option<Self> {
+                Self::from_bits(bits)
+            }
+
+            #[inline(always)]
+            fn bits(&self) -> u8 {
+                self.bits()
+            }
+        }
+    };
+}
+
+/*
+#[derive(PartialEq)]
+pub enum Interrupt {
+    CRCError = 0,
+    PkValid,
+    PkSent,
+    Ext,
+    RxFIFOAlmostFull,
+    TxFIFOAlmostEmpty,
+    TxFIFOAlmostFull,
+    FIFOError,
+    Por,
+    ChipRdy,
+    Lbd,
+    Wut,
+    Rssi,
+    Preainval,
+    Preaval,
+    Swdet,
+}
+*/
+
+rfreg! {
+    InterruptStatus1 {
+        ICRCERROR = 0,
+        IPKVALID = 1,
+        IPKSENT = 2,
+        IEXT = 3,
+        IRXFFAFULL = 4,
+        ITXFFAEM = 5,
+        ITXFFAFULL = 6,
+        IFFERR = 7
+    }
+}
+rfreg! {
+    InterruptStatus2 {
+        IPOR = 0,
+        ICHIPRDY = 1,
+        ILBDET = 2,
+        IWUT = 3,
+        IRSSI = 4,
+        IPREAINVAL = 5,
+        IPREAVAL = 6,
+        ISWDET = 7
+    }
+}
+rfreg! {
+    InterruptEnable1 {
+        ENCRCERROR = 0,
+        ENPKVALID = 1,
+        ENPKSENT = 2,
+        ENEXT = 3,
+        ENRXFFAFULL = 4,
+        ENTXFFAEM = 5,
+        ENTXFFAFULL = 6,
+        ENFFERR = 7
+    }
+}
+rfreg! {
+    InterruptEnable2 {
+        ENPOR = 0,
+        ENCHIPRDY = 1,
+        ENLBDET = 2,
+        ENWUT = 3,
+        ENRSSI = 4,
+        ENPREAINVAL = 5,
+        ENPREAVAL = 6,
+        ENSWDET = 7
+    }
+}
+rfreg! {
+    OperatingFunctionControl1 {
+        XTON = 0,
+        PLLON = 1,
+        RXON = 2,
+        TXON = 3,
+        X32KSEL = 4,
+        ENWT = 5,
+        ENLBD = 6,
+        SWRES = 7
+    }
+}
+rfreg! {
+    OperatingFunctionControl2 {
+        FFCLRTX = 0,
+        FFCLRRX = 1,
+        ENLDM = 2,
+        AUTOTX = 3,
+        RXMPK = 4,
+        ANTDIV0 = 5,
+        ANTDIV1 = 6,
+        ANTDIV2 = 7
+    }
+}
+rfreg! {
+    DataAccessControl {
+        CRC0 = 0,
+        CRC1 = 1,
+        ENCRC = 2,
+        ENPACTX = 3,
+        SKIP2PH = 4,
+        CRCDONLY = 5,
+        LSBFIRST = 6,
+        ENPACRX = 7
+    }
+}
+rfreg! {
+    HeaderControl2 {
+        PREALEN8 = 0,
+        SYNCLEN0 = 1,
+        SYNCLEN1 = 2,
+        FIXPKLEN = 3,
+        HDLEN0 = 4,
+        HDLEN1 = 5,
+        HDLEN2 = 6,
+        SKIPSYN = 7
+    }
+}
+rfreg! {
+    TxPower {
+        TXPOW0 = 0,
+        TXPOW1 = 1,
+        TXPOW2 = 2,
+        LNA_SW = 3,
+        PAPEAKLV0 = 4,
+        PAPEAKLV1 = 5,
+        PAPEAKEN = 6,
+        PAPEAKVAL = 7
+    }
+}
+
+impl TxPower {
+    fn set_tx_power(&mut self, power: u8) {
+        assert!(power <= 0x7);
+        self.remove(TXPOW2 | TXPOW1 | TXPOW0);
+        self.insert(Self::from_bits(power).unwrap());
+    }
+}
+
+rfreg! {
+    TxDataRate1 {
+        TXDR8 = 0,
+        TXDR9 = 1,
+        TXDR10 = 2,
+        TXDR11 = 3,
+        TXDR12 = 4,
+        TXDR13 = 5,
+        TXDR14 = 6,
+        TXDR15 = 7
+    }
+}
+
+impl TxDataRate1 {
+    fn from_txdr(val: u16) -> Self {
+        Self::from_bits((val >> 8) as u8).unwrap()
+    }
+}
+
+rfreg! {
+    TxDataRate0 {
+        TXDR0 = 0,
+        TXDR1 = 1,
+        TXDR2 = 2,
+        TXDR3 = 3,
+        TXDR4 = 4,
+        TXDR5 = 5,
+        TXDR6 = 6,
+        TXDR7 = 7
+    }
+}
+
+impl TxDataRate0 {
+    fn from_txdr(val: u16) -> Self {
+        Self::from_bits(val as u8).unwrap()
+    }
+}
+
+rfreg! {
+    ModulationModeControl1 {
+        ENWHITE = 0,
+        ENMANCH = 1,
+        ENMANINV = 2,
+        MANPPOL = 3,
+        ENPHPWDN = 4,
+        TXDRTSCALE = 5
+    }
+}
+rfreg! {
+    ModulationModeControl2 {
+        MODTYP0 = 0,
+        MODTYP1 = 1,
+        FD8 = 2,
+        ENINV = 3,
+        DTMOD0 = 4,
+        DTMOD1 = 5,
+        TRCLK0 = 6,
+        TRCLK1 = 7
+    }
+}
+
+#[allow(unused)]
+pub enum ModulationType {
+    Unmodulated,
+    OOK,
+    FSK,
+    GFSK,
+}
+
+#[allow(unused)]
+pub enum DataSource {
+    DirectGPIO,
+    DirectSDI,
+    FIFO,
+    PN9,
+}
+
+impl ModulationModeControl2 {
+    pub fn set_modtype(&mut self, ty: ModulationType) {
+        self.remove(MODTYP0 | MODTYP1);
+        self.insert(match ty {
+            ModulationType::Unmodulated => Self::empty(),
+            ModulationType::OOK => MODTYP0,
+            ModulationType::FSK => MODTYP1,
+            ModulationType::GFSK => MODTYP1 | MODTYP0,
+        });
+    }
+
+    pub fn set_data_source(&mut self, source: DataSource) {
+        self.remove(DTMOD0 | DTMOD1);
+        self.insert(match source {
+            DataSource::DirectGPIO => Self::empty(),
+            DataSource::DirectSDI => DTMOD0,
+            DataSource::FIFO => DTMOD1,
+            DataSource::PN9 => DTMOD1 | DTMOD0,
+        });
+    }
+}
+
+rfreg! {
+    FrequencyOffset1 {
+        FO0 = 0,
+        FO1 = 1,
+        FO2 = 2,
+        FO3 = 3,
+        FO4 = 4,
+        FO5 = 5,
+        FO6 = 6,
+        FO7 = 7
+    }
+}
+
+impl FrequencyOffset1 {
+    fn from_frequency_offset(val: u16) -> Self {
+        Self::from_bits(val as u8).unwrap()
+    }
+}
+
+rfreg! {
+    FrequencyOffset2 {
+        FO8 = 0,
+        FO9 = 1
+    }
+}
+
+impl FrequencyOffset2 {
+    fn from_frequency_offset(val: u16) -> Self {
+        Self::from_bits((val >> 8) as u8).unwrap()
+    }
+}
+
+rfreg! {
+    FrequencyBandSelect {
+        FB0 = 0,
+        FB1 = 1,
+        FB2 = 2,
+        FB3 = 3,
+        FB4 = 4,
+        HBSEL = 5,
+        SBSEL = 6
+    }
+}
+
+impl FrequencyBandSelect {
+    fn from_band(band: u8) -> Self {
+        Self::from_bits(band as u8).unwrap()
+    }
+}
+
+rfreg! {
+    CarrierFrequency1 {
+        FC8 = 0,
+        FC9 = 1,
+        FC10 = 2,
+        FC11 = 3,
+        FC12 = 4,
+        FC13 = 5,
+        FC14 = 6,
+        FC15 = 7
+    }
+}
+
+impl CarrierFrequency1 {
+    fn from_carrier(val: u16) -> Self {
+        Self::from_bits((val >> 8) as u8).unwrap()
+    }
+}
+
+rfreg! {
+    CarrierFrequency0 {
+        FC0 = 0,
+        FC1 = 1,
+        FC2 = 2,
+        FC3 = 3,
+        FC4 = 4,
+        FC5 = 5,
+        FC6 = 6,
+        FC7 = 7
+    }
+}
+
+impl CarrierFrequency0 {
+    fn from_carrier(val: u16) -> Self {
+        Self::from_bits(val as u8).unwrap()
+    }
+}
+
+pub struct Rfm22 {
+    regs: RegLogger<Box<RegRw>>,
+}
+
+impl Rfm22 {
+    pub fn new(spi: Spidev) -> Self {
+        Rfm22 { regs: RegLogger(Box::new(RfmRegs::new(spi))) }
+    }
+
+    pub fn dummy() -> Self {
+        Rfm22 { regs: RegLogger(Box::new(FakeRegs::new())) }
+    }
+
+    pub fn read<R: Rfm22Reg>(&mut self) -> io::Result<R> {
+        self.regs.read(R::regval()).map(|val| R::from_bits(val).unwrap())
+    }
+
+    pub fn write<R: Rfm22Reg>(&mut self, val: R) -> io::Result<()> {
+        self.regs.write(R::regval(), val.bits())
+    }
+
+    pub fn modify<R: Rfm22Reg, F>(&mut self, f: F) -> io::Result<()>
+        where F: FnOnce(&mut R) {
+            let mut val = self.read()?;
+            f(&mut val);
+            self.write(val)
+    }
+
+    pub fn modify_verify<R: Rfm22Reg, F>(&mut self, f: F) -> io::Result<()>
+        where F: FnOnce(&mut R) {
+            let mut val = self.read()?;
+            f(&mut val);
+            self.write_validate(val)
+    }
+
+    pub fn write_validate<R: Rfm22Reg>(&mut self, val: R) -> io::Result<()> {
+        self.write(val)?;
+        assert_eq!(val, self.read().unwrap());
+        Ok(())
+    }
+
+    pub fn set_modulation_type_and_source(&mut self, ty: ModulationType, source: DataSource) -> io::Result<()> {
+        self.modify_verify(|reg: &mut ModulationModeControl2| {
+            reg.set_modtype(ty);
+            reg.set_data_source(source);
+        })
+    }
+
+    pub fn set_tx_power(&mut self, power: u8) -> io::Result<()> {
+        self.modify_verify(|reg: &mut TxPower| {
+            reg.set_tx_power(power)
+        })
+    }
+
+    pub fn set_freq_mhz(&mut self, freq: f64) -> io::Result<()> {
+        let band = (freq as u32 - 240) / 10;
+        assert!(band <= 0x1f);
+
+        let mut bandsel = FrequencyBandSelect::from_band(band as u8);
+        if freq >= 480.0 {
+            bandsel |= HBSEL;
+        }
+
+        let foffset = 0;
+
+        let mut fcarrier = freq;
+        if bandsel.contains(HBSEL) {
+            fcarrier /= 20.0;
+        } else {
+            fcarrier /= 10.0;
+        }
+        fcarrier -= (band + 24) as f64;
+        fcarrier *= 64000.0;
+        println!("Fcarrier {}", fcarrier);
+        let fcarrier = fcarrier as u64;
+        println!("Fcarrier {}", fcarrier);
+        assert!(fcarrier <= 0xffff);
+
+        self.write_validate(bandsel)?;
+        self.write_validate(FrequencyOffset1::from_frequency_offset(foffset))?;
+        self.write_validate(FrequencyOffset2::from_frequency_offset(foffset))?;
+        self.write_validate(CarrierFrequency1::from_carrier(fcarrier as u16))?;
+        self.write_validate(CarrierFrequency0::from_carrier(fcarrier as u16))
+    }
+
+    pub fn set_data_rate_hz(&mut self, rate: f64) -> io::Result<()> {
+        let scale = rate < 30000.0;
+        self.modify_verify(|mc1: &mut ModulationModeControl1| {
+            if scale {
+                *mc1 |= TXDRTSCALE;
+            }
+        })?;
+        let exp = if scale { 16 + 5 } else { 16 };
+        let txdr = rate * (1 << exp) as f64;
+        let txdr = (txdr / 1000000.0) as u64;
+        assert!(txdr <= 0xffff);
+        self.write_validate(TxDataRate1::from_txdr(txdr as u16))?;
+        self.write_validate(TxDataRate0::from_txdr(txdr as u16))
+    }
+
+    fn clear_tx_fifo(&mut self) -> io::Result<()> {
+        self.modify_verify(|reg: &mut OperatingFunctionControl2| {
+            reg.insert(FFCLRTX);
+        })?;
+        self.modify_verify(|reg: &mut OperatingFunctionControl2| {
+            reg.remove(FFCLRTX);
+        })
+    }
+
+    fn write_tx_fifo(&mut self, buf: &[u8]) -> io::Result<()> {
+        self.regs.burst_write(Rfm22RegVal::FIFOAccess as u8, buf)
+    }
+
+    fn transmit(&mut self) -> io::Result<()> {
+        self.modify(|reg: &mut OperatingFunctionControl1| {
+            reg.insert(TXON)
+        })
+    }
+
+    fn transmit_large<'a, I: IntoIterator<Item=u8>>(&mut self, iter: I) -> io::Result<()> {
+        // The almost empty IRQ happens at 4 by default. Leave some extra space
+        // so we can never fill the FIFO completely. This could probably be
+        // exactly 4, but I don't know how the boundary conditions work in HW.
+        let mut buf = Vec::with_capacity(FIFO_SIZE - 10);
+        let capacity = buf.capacity();
+        let mut iter = iter.into_iter().peekable();
+
+        println!("Iter[0] {:?}", iter.peek());
+        buf.extend(iter.by_ref().take(capacity));
+        if buf.len() == 0 {
+            println!("Zero length transmit!");
+            return Ok(());
+        }
+        self.clear_tx_fifo()?;
+        self.setup_irq()?;
+        // Clear pending IRQs
+        self.get_irq()?;
+        self.write_tx_fifo(&buf)?;
+        // Start transmitter
+        self.transmit()?;
+        let mut irq = self.get_irq()?;
+        while let Some(_) = iter.peek() {
+            let mut timeout = 0;
+            irq = self.get_irq()?;
+            while !irq.contains(ITXFFAEM) {
+                thread::sleep(Duration::from_millis(1)); // XXX hardcoded
+                irq = self.get_irq()?;
+                timeout += 1;
+                if timeout > 1000 {
+                    println!("Timed out");
+                    return Ok(());
+                }
+            }
+            buf.clear();
+            buf.extend(iter.by_ref().take(capacity));
+            self.write_tx_fifo(&buf)?;
+        }
+        let mut timeout = 0;
+        while !irq.contains(IPKSENT) {
+            thread::sleep(Duration::from_millis(1)); // XXX hardcoded
+            irq = self.get_irq()?;
+            timeout += 1;
+            if timeout > 1000 {
+                println!("Timed out");
+                return Ok(());
+            }
+        }
+        Ok(())
+    }
+
+    pub fn transmit_bitstream<'a, I: IntoIterator<Item=bool>>(&mut self, iter: I) -> io::Result<()> {
+        struct BitsToBytes<I: Iterator<Item=bool>>(I);
+
+        impl<I: Iterator<Item=bool>> Iterator for BitsToBytes<I> {
+            type Item = u8;
+
+            fn next(&mut self) -> Option<Self::Item> {
+                let mut val = 0;
+                if let Some(bit) = self.0.next() {
+                    if bit {
+                        val |= 1 << 7;
+                    }
+                } else {
+                    return None;
+                }
+                // Finish the byte if there was at least 1 bit
+                for idx in (0..7).into_iter().rev() {
+                    if let Some(bit) = self.0.next() {
+                        if bit {
+                            val |= 1 << idx;
+                        }
+                    }
+                }
+                Some(val)
+            }
+        }
+
+        self.transmit_large(BitsToBytes(iter.into_iter()))
+    }
+
+    pub fn is_transmitting(&mut self) -> io::Result<bool> {
+        self.read().map(|val: OperatingFunctionControl1| val.contains(TXON))
+    }
+
+    fn get_irq(&mut self) -> io::Result<InterruptStatus1> {
+        self.read()
+    }
+
+    fn setup_irq(&mut self) -> io::Result<()> {
+        self.write_validate(ENPKSENT | ENTXFFAEM)
+    }
+
+    pub fn init(&mut self) {
+        self.write_validate(XTON | PLLON).unwrap();
+    }
+}
+
