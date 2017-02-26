2255fbed5323b453ccc54817ae1f551105c63f80
Tyler Hall <tylerwhall@gmail.com>
2017-02-26 17:07:40 -0500

    21-bit protocol and arg handling

--- src/main.rs
+++ src/main.rs
@@ -24,21 +24,26 @@ use rfm::*;
 
 enum FanPkt {
     Dumb(FanPkt12),
+    Smart(FanPkt21),
 }
 
 impl FanPkt {
     fn transmit(&self, rf: &mut Rfm22) {
-        match *self {
-            FanPkt::Dumb(ref pkt) => {
-                let bits = repeat(FanExpand::new(repeat(false).take(1) // Start bit
-                                                 .chain(pkt.into_iter()))
-                                  .chain(std::iter::repeat(false).take(11 * 3))) // 11ms pause between commands. 1/3ms symbol period
-                    .cycle()
-                    .take(20)
-                    .flat_map(|i| i);
+        fn send_pkt<I: IntoIterator<Item = bool>>(rf: &mut Rfm22, iter: I, count: usize)
+            where I::IntoIter: Clone
+        {
+            let bits = repeat(FanExpand::new(repeat(false).take(1) // Start bit
+                                             .chain(iter.into_iter()))
+                              .chain(std::iter::repeat(false).take(11 * 3))) // 11ms pause between commands. 1/3ms symbol period
+                .cycle()
+                .take(count)
+                .flat_map(|i| i);
+            rf.transmit_bitstream(bits).unwrap();
+        }
 
-                rf.transmit_bitstream(bits).unwrap();
-            }
+        match *self {
+            FanPkt::Dumb(ref pkt) => send_pkt(rf, pkt, 20),
+            FanPkt::Smart(ref pkt) => send_pkt(rf, pkt, 30),
         }
     }
 }
@@ -139,6 +144,133 @@ fn fan12_serializer() {
     }
 }
 
+fn reverse_nibble(n: u8) -> u8 {
+    (n & (1 << 3)) >> 3 | (n & (1 << 2)) >> 1 | (n & (1 << 1)) << 1 | (n & (1 << 0)) << 3
+}
+
+#[test]
+fn test_reverse_nibble() {
+    assert_eq!(0x8, reverse_nibble(0x1));
+    assert_eq!(0x4, reverse_nibble(0x2));
+    assert_eq!(0x2, reverse_nibble(0x4));
+    assert_eq!(0x1, reverse_nibble(0x8));
+    assert_eq!(0x7, reverse_nibble(0xe));
+}
+
+#[repr(u8)]
+#[derive(Copy, Clone)]
+enum FanState21 {
+    Off = 0x3,
+    Low = 0x0,
+    Med = 0x1,
+    High = 0x2,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct FanPkt21 {
+    data0: u8,
+    data1: u8,
+    chksum: u8,
+}
+
+impl FanPkt21 {
+    fn new(addr: u8, brightness: f64, fan: FanState21) -> Self {
+        const BRIGHTNESS_MAX: u8 = 62;
+        // Fan seems to reject commands with brightness < ~30%
+        const BRIGHTNESS_MIN: u8 = 19;
+        assert!(brightness >= 0.0 && brightness <= 1.0);
+        // Scale brightness.
+        let brightness = if brightness == 0.0 {
+            // Max value indicates off
+            63
+        } else {
+            ((BRIGHTNESS_MAX - BRIGHTNESS_MIN) as f64 * brightness) as u8 + BRIGHTNESS_MIN
+        };
+        let data0 = 0x7 << 5 | reverse_nibble(addr) << 1 | 1;
+        let data1 = brightness << 2 | fan as u8;
+        let chksum = (data0 >> 4) + (data0 & 0xf) + (data1 >> 4) + (data1 & 0xf) + 3;
+        FanPkt21 {
+            data0: data0,
+            data1: data1,
+            chksum: chksum & 0xf,
+        }
+    }
+}
+
+impl<'a> IntoIterator for &'a FanPkt21 {
+    type Item = bool;
+    type IntoIter = FanPkt21Bits<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        FanPkt21Bits::new(self)
+    }
+}
+
+#[derive(Clone)]
+struct FanPkt21Bits<'a> {
+    pkt: &'a FanPkt21,
+    count: u8,
+}
+
+impl<'a> FanPkt21Bits<'a> {
+    fn new(pkt: &'a FanPkt21) -> Self {
+        FanPkt21Bits {
+            pkt: pkt,
+            count: 0,
+        }
+    }
+}
+
+impl<'a> Iterator for FanPkt21Bits<'a> {
+    type Item = bool;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let ret = match self.count {
+            0...7 => Some(self.pkt.data0 & (1 << (7 - (self.count - 0))) != 0),
+            8...15 => Some(self.pkt.data1 & (1 << (7 - (self.count - 8))) != 0),
+            16 => Some(true),
+            17...20 => Some(self.pkt.chksum & (1 << (3 - (self.count - 17))) != 0),
+            _ => return None,
+        };
+        self.count += 1;
+        ret
+    }
+}
+
+#[test]
+fn fan21_serializer() {
+    fn from_iter<I: Iterator<Item = bool>>(mut iter: I) -> u8 {
+        // Three high bits
+        assert_eq!(iter.next().unwrap(), true);
+        assert_eq!(iter.next().unwrap(), true);
+        assert_eq!(iter.next().unwrap(), true);
+        let addr = if iter.next().unwrap() { 1 << 0 } else { 0 } |
+                   if iter.next().unwrap() { 1 << 1 } else { 0 } |
+                   if iter.next().unwrap() { 1 << 2 } else { 0 } |
+                   if iter.next().unwrap() { 1 << 3 } else { 0 };
+        // High bit
+        assert_eq!(iter.next().unwrap(), true);
+        // State
+        for _ in 0..8 {
+            iter.next().unwrap();
+        }
+        // High bit
+        assert_eq!(iter.next().unwrap(), true);
+        // Chksum
+        for _ in 0..4 {
+            iter.next().unwrap();
+        }
+        assert!(iter.next().is_none());
+        addr
+    }
+    for addr in 0..16 {
+        for state in [FanState21::Off].iter() {
+            let pkt = FanPkt21::new(addr, 0.0, *state);
+            assert_eq!(addr, from_iter(pkt.into_iter()));
+        }
+    }
+}
+
 #[derive(Clone)]
 enum FanExpandState {
     Start,
@@ -222,14 +354,22 @@ fn arg_app<'a, 'b>() -> App<'a, 'b> {
             .arg(Arg::with_name("command")
                 .index(1)
                 .required(true)
-                .help(concat!("light\tToggle the light\n",
-                              "off\tFan off\n",
-                              "low\tFan low\n",
-                              "medum\tFan medium\n",
-                              "high\tFan high\n"))))
+                .help("light\tToggle the light\n\
+                       off\tFan off\n\
+                       low\tFan low\n\
+                       medum\tFan medium\n\
+                       high\tFan high\n")))
         .subcommand(SubCommand::with_name("smart")
             .about("Send a 21-bit command. For fans with an LCD in the remote where the remote \
-                    keeps the dimmer state."))
+                    keeps the dimmer state.")
+            .arg(Arg::with_name("fan")
+                .index(1)
+                .required(true)
+                .help("off\tFan off\nlow\tFan low\nmedum\tFan medium\nhigh\tFan high\n"))
+            .arg(Arg::with_name("brightness")
+                .index(2)
+                .required(true)
+                .help("Light brightness percentage (0-100)")))
         .setting(AppSettings::SubcommandRequired)
 }
 
@@ -274,7 +414,35 @@ fn main() {
         };
         FanPkt::Dumb(FanPkt12::new(0x9, cmd))
     } else if let Some(matches) = matches.subcommand_matches("smart") {
-        unimplemented!()
+        let fan = match matches.value_of("fan").unwrap() {
+            "off" => FanState21::Off,
+            "low" => FanState21::Low,
+            "medium" => FanState21::Med,
+            "high" => FanState21::High,
+            _ => {
+                clap::Error::with_description("Invalid fan state. Possible values: \
+                                               off|low|medium|high",
+                                              clap::ErrorKind::UnknownArgument)
+                    .exit();
+            }
+        };
+        let brightness = matches.value_of("brightness")
+            .unwrap()
+            .parse::<u8>()
+            .map(|brightness| {
+                if brightness > 100 {
+                    clap::Error::with_description("Brightness out of range 0-100",
+                                                  clap::ErrorKind::ValueValidation)
+                        .exit();
+                }
+                brightness as f64 / 100.0
+            })
+            .unwrap_or_else(|_| {
+                clap::Error::with_description("Unable to parse brightness as integer",
+                                              clap::ErrorKind::InvalidValue)
+                    .exit();
+            });
+        FanPkt::Smart(FanPkt21::new(0xe, brightness, fan))
     } else {
         // Arg parser enforces subcommand requirement
         unreachable!()
